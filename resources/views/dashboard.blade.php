b596e5659d9c4aae63dc5070b658b010cc964058
Ahmad Nauval Syahputra <anvalputra@gmail.com>
2023-12-02 19:59:36 +0700

    resources: views: Dashboard add title names

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('chatify.name') }}</title>
+    <title>{{ config('chatify.name') }} - Dashboard</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
